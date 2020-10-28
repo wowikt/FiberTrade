68c823ab88a04d6784d4b5fe7ea2b61f6101e342
vtimkin <vtimkin@unitarius.ru>
2020-10-28 12:18:16 +0700

    Added controllers to ASP.NET MVC project

---
+++ FifteenGameAspNet/FifteenGameAspNet/Controllers/DialogController.cs
@@ -0,0 +1,26 @@
+﻿using FifteenGameAspNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FifteenGameAspNet.Controllers
+{
+    public class DialogController : Controller
+    {
+        // GET: Dialog
+        public ActionResult Index()
+        {
+            ViewBag.GreetingMessage = "Представьтесь, пожалуйста:";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(DialogViewModel model)
+        {
+            return View("Reply", model);
+        }
+    }
+}
---
+++ FifteenGameAspNet/FifteenGameAspNet/Models/DialogViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FifteenGameAspNet.Models
+{
+    public class DialogViewModel
+    {
+        [Required(ErrorMessage ="Представьтесь, пожалуйста")]
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}

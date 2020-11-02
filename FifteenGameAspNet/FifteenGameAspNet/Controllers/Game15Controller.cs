5f64db00f36d720c6ee0925c044384a06f419a25
vtimkin <vtimkin@unitarius.ru>
2020-11-02 13:38:25 +0700

    Added game 15 web view.

---
+++ FifteenGameAspNet/FifteenGameAspNet/Controllers/Game15Controller.cs
@@ -0,0 +1,61 @@
+﻿using FifteenGame.Business.Models;
+using FifteenGame.Business.Services;
+using FifteenGameAspNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FifteenGameAspNet.Controllers
+{
+    public class Game15Controller : Controller
+    {
+        private GameService Service
+        {
+            get
+            {
+                if (Session["Service"] == null)
+                {
+                    var service = new GameService();
+                    service.Initialize();
+                    service.Shuffle();
+                    Session["Service"] = service;
+                    return service;
+                }
+
+                return (GameService)Session["Service"];
+            }
+        }
+
+        public Game15Controller()
+        {
+        }
+
+        // GET: Game15
+        public ActionResult Index()
+        {
+            var viewModel = new Game15ViewModel();
+            viewModel.BuildFromService(Service);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(MoveDirection direction)
+        {
+            Service.MakeMove(direction);
+            //var viewModel = new Game15ViewModel();
+            //viewModel.BuildFromService(Service);
+            //return View(viewModel);
+            return new JsonResult();
+        }
+
+        public ActionResult MakeMove(MoveDirection direction)
+        {
+            Service.MakeMove(direction);
+            var viewModel = new Game15ViewModel();
+            viewModel.BuildFromService(Service);
+            return View("Index", viewModel);
+        }
+    }
+}
---
+++ FifteenGameAspNet/FifteenGameAspNet/Models/Game15ViewModel.cs
@@ -0,0 +1,58 @@
+﻿using FifteenGame.Business.Models;
+using FifteenGame.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FifteenGameAspNet.Models
+{
+    public class Game15ViewModel
+    {
+        public int RowCount { get; set; }
+
+        public int ColumnCount { get; set; }
+
+        public ButtonViewModel[,] Buttons { get; set; }
+
+        public void BuildFromService(GameService service)
+        {
+            var field = service.GameField;
+            RowCount = GameField.RowCount;
+            ColumnCount = GameField.ColumnCount;
+            Buttons = new ButtonViewModel[RowCount, ColumnCount];
+
+            for (int row = 0; row < RowCount; row++)
+            {
+                for (int col = 0; col < ColumnCount; col++)
+                {
+                    var newButton = new ButtonViewModel
+                    {
+                        Value = field[row, col]
+                    };
+
+                    newButton.IsVisible = (row != service.GameField.EmptyCellRow) || (col != service.GameField.EmptyCellColumn);
+                    newButton.MoveDirection = MoveDirection.None;
+                    if (row == service.GameField.EmptyCellRow && col == service.GameField.EmptyCellColumn - 1)
+                    {
+                        newButton.MoveDirection = MoveDirection.Right;
+                    }
+                    else if (row == service.GameField.EmptyCellRow && col == service.GameField.EmptyCellColumn + 1)
+                    {
+                        newButton.MoveDirection = MoveDirection.Left;
+                    }
+                    else if (row == service.GameField.EmptyCellRow - 1 && col == service.GameField.EmptyCellColumn)
+                    {
+                        newButton.MoveDirection = MoveDirection.Down;
+                    }
+                    else if (row == service.GameField.EmptyCellRow + 1 && col == service.GameField.EmptyCellColumn)
+                    {
+                        newButton.MoveDirection = MoveDirection.Up;
+                    }
+
+                    Buttons[row, col] = newButton;
+                }
+            }
+        }
+    }
+}
